119e7039dbf78d4cede7b44b2eee1eb06aa624f3
kandoit7 <jmin8884@gmail.com>
2016-09-13 11:19:54 +0900

    Update recording.js

--- javascripts/recording.js
+++ javascripts/recording.js
@@ -77,56 +77,54 @@ function DeviceSelect(e) {
 			audioInputSelect[selector].parentNode.replaceChild(newInputSelector, audioInputSelect[selector]);
 		}
 	}
-}
-
-//Audio recording check
-function initAudio(index) {
-
-  	var audioSource = index.value;
-	var idconfirm = index.parentNode;
-	console.log(idconfirm);
-	var recordCloud = document.createElement('a');
-	var audioRecorder = null;
 	
-	function gotStream(stream) {
+	//Audio recording check
+	function initAudio(index) {
+
+	  	var audioSource = index.value;
+		var idconfirm = e.parentNode.parentNode;
+		var recordCloud = document.createElement('a');
+		var audioRecorder = null;
+		
+		function gotStream(stream) {
+				
+			// Create an AudioNode from the stream.
+			var realAudioInput = audioContext.createMediaStreamSource(stream);
+			var audioInput = realAudioInput;
 			
-		// Create an AudioNode from the stream.
-		var realAudioInput = audioContext.createMediaStreamSource(stream);
-		var audioInput = realAudioInput;
+			var inputPoint = audioContext.createGain();
+			inputPoint.gain.value = 1.0;
+			audioInput.connect(inputPoint);
+			
+			var analyserNode = audioContext.createAnalyser();
+			analyserNode.fftSize = 2048;
+			inputPoint.connect( analyserNode );
+			
+			audioRecorder = new Recorder( inputPoint ); // this fuck what the fuck
+			// speak / headphone feedback initial settings
+			
+			inputPoint.connect(audioContext.destination);
+			return audioRecorder;
+		}
 		
-		var inputPoint = audioContext.createGain();
-		inputPoint.gain.value = 1.0;
-		audioInput.connect(inputPoint);
+		var constraints = {
+			audio: { deviceId: audioSource ? {exact: audioSource} : undefined}
+		};
 		
-		var analyserNode = audioContext.createAnalyser();
-		analyserNode.fftSize = 2048;
-		inputPoint.connect( analyserNode );
-		
-		audioRecorder = new Recorder( inputPoint ); // this fuck what the fuck
-		// speak / headphone feedback initial settings
-		
-		inputPoint.connect(audioContext.destination);
-		return audioRecorder;
+		navigator.mediaDevices.getUserMedia(constraints)
+		.then(gotStream)
+		.then(function() { 
+			idconfirm.src = audioRecorder;
+		})
+		.catch(handleError);
 	}
-	
-	var constraints = {
-		audio: { deviceId: audioSource ? {exact: audioSource} : undefined}
-	};
-	
-	navigator.mediaDevices.getUserMedia(constraints)
-	.then(gotStream)
-	.then(function() { 
-		idconfirm.src = audioRecorder;
-	})
-	.catch(handleError);
 }
-
-
 
 // function for many Selector Element 
 function changeAudioDestination(event) {
 	
 	var InputSelector = event.path[0];
+	console.log(InputSelector);
 	initAudio(InputSelector);
 }
 
